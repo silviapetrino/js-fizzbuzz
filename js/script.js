b48725d63e6605faa1c45029dc3d1526f9c55641
silviapetrino <petrino.silvia@gmail.com>
2023-09-11 16:28:42 +0200

    [add] dom interaction

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
 
 const myRow = document.getElementById("row");
 
+
 for (let i = 1; i <= 100; i++) {
 
-const div = document.createElement("div");
-div.append (i);
-myRow.append(div);
-div.classList.add("square");
+const square = document.createElement("div");
+square.append (i);
+myRow.append(square);
+square.classList.add("square-sp");
+const fizz = "fizz";
+const buzz = "buzz";
+
+if (i % 3 === 0 && i % 5 === 0) {
+  square.classList.add("bg-primary");
+  square.innerHTML = fizz + buzz;
+}
+
+else if (i % 3 === 0) {
+  square.classList.add("bg-success");
+  square.innerHTML = fizz;
+}
+
+else if (i % 5 === 0) {
+  square.classList.add("bg-danger");
+  square.innerHTML = buzz;
+}
+
+else {
+  square.classList.add("bg-warning");
+}
+
 }
 
 
-
-
-// if (i % 3 === 0 && i % 5 === 0) {
-    
-//   console.log("square " + i);
-
-
-// } else if (i % 3 === 0) {
-//   console.log("tre " + i);
-
-
-// } else if (i % 5 === 0) {
-//   console.log("cinque " + i);
-
-
-// } else {
-//   console.log(i);
-// }
