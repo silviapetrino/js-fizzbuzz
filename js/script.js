985e168a232f422accb664e15b616421b6de2a1f
silviapetrino <petrino.silvia@gmail.com>
2023-09-11 16:08:39 +0200

    [add] squares

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 
+const myRow = document.getElementById("row");
 
 for (let i = 1; i <= 100; i++) {
 
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("tutti e due " + i);
-
-
-  } else if (i % 3 === 0) {
-    console.log("tre " + i);
-
-
-  } else if (i % 5 === 0) {
-    console.log("cinque " + i);
-
-    
-  } else {
-    console.log(i);
-  }
+const div = document.createElement("div");
+div.append (i);
+myRow.append(div);
+div.classList.add("square");
 }
+
+
+
+
+// if (i % 3 === 0 && i % 5 === 0) {
+    
+//   console.log("square " + i);
+
+
+// } else if (i % 3 === 0) {
+//   console.log("tre " + i);
+
+
+// } else if (i % 5 === 0) {
+//   console.log("cinque " + i);
+
+
+// } else {
+//   console.log(i);
+// }
