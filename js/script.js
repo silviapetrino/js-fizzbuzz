282686ed6c9a54c87d7587a154844824e29a3a78
silviapetrino <petrino.silvia@gmail.com>
2023-09-11 15:22:47 +0200

    [fix] cicle

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 
+
 for (let i = 1; i <= 100; i++) {
-  
+
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("tutti e due " + i);
 
+
   } else if (i % 3 === 0) {
     console.log("tre " + i);
-  }
 
-  else if (i % 5 === 0) {
-  console.log("cinque " + i);
-  }
 
+  } else if (i % 5 === 0) {
+    console.log("cinque " + i);
+
+    
+  } else {
+    console.log(i);
+  }
 }
