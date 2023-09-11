64d6e728b794f36bea38be8f100d414268417e99
silviapetrino <petrino.silvia@gmail.com>
2023-09-11 16:35:22 +0200

    [add] console.log

--- js/script.js
+++ js/script.js
@@ -14,20 +14,24 @@ const buzz = "buzz";
 if (i % 3 === 0 && i % 5 === 0) {
   square.classList.add("bg-primary");
   square.innerHTML = fizz + buzz;
+  console.log("multipli di tre e cinque, casella n." + " " + i);
 }
 
 else if (i % 3 === 0) {
   square.classList.add("bg-success");
   square.innerHTML = fizz;
+  console.log("multipli di tre, casella n." + " " + i);
 }
 
 else if (i % 5 === 0) {
   square.classList.add("bg-danger");
   square.innerHTML = buzz;
+  console.log("multipli di cinque, casella n." + " " + i);
 }
 
 else {
   square.classList.add("bg-warning");
+  console.log(i);
 }
 
 }
